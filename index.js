96940c112f24d474683716e862bc0d8baa91d9b4
João Campos <joaog.taborda.campos@gmail.com>
2022-02-17 18:02:45 -0300

    Inicio da tarefa

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const filmes = [
+  {
+    nome: 'A cura',
+    genero: 'Suspense',
+    elenco: [
+      {
+        ator: 'Dane DeHaan'
+      },
+      {
+        ator: 'Jason Isaacs'
+      }
+    ]
+  },
+  {
+    nome: 'A caverna',
+    genero: 'Ação e aventura',
+    elenco: [
+      {
+        ator: 'Andrew Wilson'
+      },
+      {
+        ator: 'Cassidy Gifford'
+      }
+    ]
+  }
+]
+
+const filmesEmJson = JSON.stringify(filmes)
+
+app.get('/filmes', (req, res) => {
+  res.send(filmesEmJson)
+})
